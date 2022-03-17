3018ebe8892c61d0467da7ae8ec4de0e0f1d63f0
Collins Kiplangat <collinskiplangat3.com>
2022-03-17 17:11:12 +0300

    final comit for today

--- src/App.js
+++ src/App.js
@@ -3,33 +3,54 @@ import axios from 'axios';
 import './App.css';
 
 const App=()=>{
-  const APP_ID="f6412446";
-  const APP_KEY="a50f4012d2a24c17192f3073a3566333";
-  const url=" https://api.edamam.com/api/recipes/v2";
-  const exampleReq=`https://api.edamam.com/api/recipes/v2/0123456789abcdef0123456789abcdef?app_id=${APP_ID}&app_key=${APP_KEY}`;
 
-  const[counter,setCounter]=useState(0);
-  useEffect(async()=>{
-    fetRecipe();
+  
+  const[container,setContainer]=useState('');
+  const[endpoints,setEndpoints]=useState('');
 
-    console.log("useRffect has been run");
-  },[]);
+  useEffect(()=>{
+    fetchRecipe();
+  },[endpoints])
 
-  const fetRecipe=async()=>{
-    const response=await fetch(exampleReq)
-    const data=await response.json()
-    console.log(data)
+
+  const fetchRecipe=()=>{
+    fetch(`https://edamam-food-and-grocery-database.p.rapidapi.com/parser?ingr=${endpoints}`, {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "edamam-food-and-grocery-database.p.rapidapi.com",
+      "x-rapidapi-key": "f7986b70ecmsh3d98881202f754ep1e0190jsnb0a27d39691c"
+    }
+  })
+  .then(response => {
+    console.log(response.json())
+  })
+  .then(data=>{
+    setContainer(data.hints)
+  })
+  .catch(err => {
+    console.error(err);
+  });
+
+
   }
 
+  
+  const onChangeHandler=(e)=>{
+    setEndpoints(e.target.value)
+  }
 
+  const submitHandlet=(e)=>{
+    e.preventDefault();
+  }
 
   return(
     <div className='App'>
       <h2>Hello world</h2>
-      <form className='form' action="submit">
-        <input className='input' type="text" />
+      <form className='form' onSubmit={submitHandlet}>
+        <input className='input' type="text" onChange={onChangeHandler} value={endpoints}/>
         <button className='button' type='submit'>search</button>
       </form>
+      <p>food available{container}</p>
     </div>
   );
 }
