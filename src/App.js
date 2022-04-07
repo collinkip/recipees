99eb262dafb241e73a6a3815156105614f16b50f
Collins Kiplangat <collinskiplangat3.com>
2022-04-07 20:24:35 +0300

    new pages added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { render } from '@testing-library/react';
 import React,{useEffect,useState} from 'react';
 import './App.css';
 import Navbar from './navbar';
@@ -7,11 +6,13 @@ import Product from './products';
 
 const App=()=>{
   const [recipes,setRecipes]=useState([]);
+  const [search,setSearch]=useState("");
+  const [query,setQuery]=useState();
   useEffect(()=>{
     fetchRecipe();
-  },[]);
+  },[query]);
   const fetchRecipe= async()=>{
-    const response =await fetch(`https://edamam-food-and-grocery-database.p.rapidapi.com/parser?ingr=chicken`, {
+    const response =await fetch(`https://edamam-food-and-grocery-database.p.rapidapi.com/parser?ingr=${query}`, {
       "method": "GET",
       "headers": {
         "x-rapidapi-host": "edamam-food-and-grocery-database.p.rapidapi.com",
@@ -25,23 +26,31 @@ const App=()=>{
     setRecipes(data.hints)
     
   }  
-  const onChangeHandler=(e)=>{
+  // events
+
+  const updateSearch=e=>{
+    setSearch(e.target.value);
+    console.log(search)
   }
 
-  const submitHandler=(e)=>{
+  const getSearch=e=>{
     e.preventDefault();
+    setQuery(search);
   }
-
   return(
     <div className='App'>
        <Navbar/>
       <h2>Hello world</h2>
-      <form className='form' onSubmit={submitHandler}>
-        <input className='input' type="text" onChange={onChangeHandler}/>
-        <button className='button' type='submit'>search</button>
+      <form className='form' onSubmit={getSearch}>
+        <input className='input' type="text" value={search} onChange={updateSearch}/>
+        <button className='button'>search</button>
       </form>
       {recipes.map(recipe=>(
-        <Product title={recipe.food.label} image={recipe.food.image} category={recipe.food.category}/>
+        <Product
+        key={recipe.food.uri}
+         title={recipe.food.label} 
+         image={recipe.food.image} 
+         category={recipe.food.category}/>
       ))}
       <Footer/>
     </div>
--- src/products.js
+++ src/products.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
 const Product=({title,image,category})=>{
+
     return(
+        
         <div><h2>{title}</h2>
         <p>
             {category}
